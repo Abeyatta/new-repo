1a7e85588d32a856834c9383090e365e5ede6d57
Abey Assefa <abeyatta@yahoo.com>
2020-09-14 17:04:51 +0100

    add even sum function

--- pycharm.py
+++ pycharm.py
@@ -1,2 +1,10 @@
 # first print out
 print('Hello World')
+def sum_even(n):
+    sum_ev = 0
+    for i in range(1, n):
+        if i % 2 == 0:
+           sum_ev += i
+    return sum_ev
+
+sum_even(17)
